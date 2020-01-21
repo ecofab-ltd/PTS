c2536e13d1854300d346a5d61b94aacd8744bb80
ecofab-pts <ecofab.pts@gmail.com>
2020-01-21 09:55:46 +0600

    Model getProductionReportLive() Wash Send Option Update

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -2403,7 +2403,7 @@ class Dashboard_model extends CI_Model {
     public function getProductionReportLive($where)
     {
         $sql="SELECT t1.line_id,t1.packing_date_time,  t1.total_cut_input_qty, 
-              t1.count_input_line_qc_pass, t1.count_mid_line_qc_pass, t1.count_end_line_qc_pass, 
+              t1.count_input_line_qc_pass, t1.count_mid_line_qc_pass, t1.count_end_line_qc_pass, t1.count_wash_send, 
               t1.count_washing_pass, t1.count_packing_pass, t1.count_carton_pass, t1.total_wh_qa, 
               t2.*, t3.responsible_line, t4.so_fail_count
               FROM
@@ -2413,22 +2413,24 @@ class Dashboard_model extends CI_Model {
               (SELECT po_no,so_no,item,quality,color,style_name,style_no,
               purchase_order,brand,ex_factory_date,packing_date_time,line_id,
               COUNT(sent_to_production_date_time) as total_cut_input_qty,
-               COUNT(line_input_date_time) as count_input_line_qc_pass,
-                 COUNT(mid_line_qc_date_time) as count_mid_line_qc_pass,
-                 COUNT(end_line_qc_date_time) as count_end_line_qc_pass,
-                  COUNT(washing_status) as count_washing_pass, 
-             	COUNT(packing_status) as count_packing_pass,  
-          		COUNT(carton_status) as count_carton_pass,
-                COUNT(warehouse_qa_type) as total_wh_qa
+              COUNT(line_input_date_time) as count_input_line_qc_pass,
+              COUNT(mid_line_qc_date_time) as count_mid_line_qc_pass,
+              COUNT(end_line_qc_date_time) as count_end_line_qc_pass,
+              COUNT(is_going_wash) as count_wash_send, 
+              COUNT(washing_status) as count_washing_pass, 
+              COUNT(packing_status) as count_packing_pass,  
+          	  COUNT(carton_status) as count_carton_pass,
+              COUNT(warehouse_qa_type) as total_wh_qa
            FROM (
               SELECT
                 so_no,po_no,item,quality,color,purchase_order,brand,ex_factory_date,packing_date_time,style_name,style_no,line_id,
-               CASE WHEN  access_points=4 AND access_points_status=4 THEN end_line_qc_date_time END end_line_qc_date_time,    
-               CASE WHEN sent_to_production=1 THEN sent_to_production_date_time END sent_to_production_date_time,
+                CASE WHEN  access_points=4 AND access_points_status=4 THEN end_line_qc_date_time END end_line_qc_date_time,    
+                CASE WHEN sent_to_production=1 THEN sent_to_production_date_time END sent_to_production_date_time,
                 CASE WHEN access_points>=3 AND access_points_status in (1, 4) THEN mid_line_qc_date_time END mid_line_qc_date_time,
                 CASE WHEN access_points>=2 AND access_points_status in (1, 4) THEN line_input_date_time END line_input_date_time,
                 CASE WHEN packing_status = 1 THEN packing_status END packing_status,
-                 CASE WHEN carton_status = 1 THEN carton_status END carton_status,
+                CASE WHEN carton_status = 1 THEN carton_status END carton_status,
+                CASE WHEN is_going_wash = 1 THEN is_going_wash END is_going_wash,
                 CASE WHEN washing_status = 1 THEN washing_status END washing_status,
                 CASE WHEN warehouse_qa_type != 0 THEN warehouse_qa_type END warehouse_qa_type 
               FROM vt_few_days_po_pcs    
