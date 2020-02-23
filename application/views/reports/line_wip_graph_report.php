4d7a39ae51043c45ab0500cdf4ba215a2d2caedd
ecofab-pts <ecofab.pts@gmail.com>
2020-02-23 17:31:17 +0600

    Update-3 2020-02-23

--- application/views/reports/line_wip_graph_report.php
+++ application/views/reports/line_wip_graph_report.php
@@ -67,6 +67,7 @@
                 title: {
                     text: "CUT TABLE REPORT: <?php echo $date;?>"
                 },
+                dataPointWidth: 80,
                 axisX: {
                     valueFormatString: ""
                 },
