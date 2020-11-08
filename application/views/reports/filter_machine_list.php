4b4e35db6527e506e74d614edb1cca97a9d470b5
ecofab-pts <ecofab.pts@gmail.com>
2020-11-08 10:47:17 +0600

    Machine Maintenance Module Update

--- application/views/reports/filter_machine_list.php
+++ application/views/reports/filter_machine_list.php
@@ -5,16 +5,32 @@ $sl=1;
 foreach ($machine_list as $v){ ?>
 
     <tr>
-        <td class="center"><?php echo $sl; $sl++;?></td>
-        <td class="center"><?php echo $v['machine_no'];?></td>
-        <td class="center"><?php echo $v['machine_description'];?></td>
-        <td class="center"><?php echo $v['model_no'];?></td>
-        <td class="center"><?php echo $v['line_code'];?></td>
-        <td class="center"><?php echo $v['other_location'];?></td>
-        <td class="center"><?php echo ($v['status'] == 1 ? 'ACTIVE' : ($v['status'] == 0 ? 'INACTIVE' : '') );?></td>
-        <td class="center"><?php echo ($v['status'] == 1 ? 'RUNNING' : ($v['status'] == 2 ? 'UNDER MAINTENANCE' : '') );?></td>
         <td class="center">
-            <a class="btn btn-warning" href="" title="EDIT"><i class="fa fa-edit"></i></a>
+            <?php echo $sl; $sl++;?>
+            <input type="hidden" required="required" readonly="readonly" id="machine_id_<?php echo $k;?>" value="<?php echo $v['id'];?>">
+        </td>
+        <td class="center"><?php echo $v['machine_no'];?></td>
+        <td class="center"><?php echo $v['machine_name'];?></td>
+        <td class="center"><?php echo $v['machine_model'];?></td>
+        <td class="center"><?php echo $v['brand'];?></td>
+        <td class="center"><?php echo $v['line_code'];?></td>
+        <td class="center"><?php echo $v['location_name'];?></td>
+        <td class="center"><?php echo ($v['status'] == 1 ? 'ACTIVE' : ($v['status'] == 0 ? 'INACTIVE' : '') );?></td>
+        <td class="center">
+            <?php
+            if($v['service_status'] == 0){
+                echo 'Out of Service';
+            }
+            if($v['service_status'] == 1){
+                echo 'Running';
+            }
+            if($v['service_status'] == 2){
+                echo 'Under Maintenance';
+            }
+            if($v['service_status'] == 3){
+                echo 'Idle';
+            }
+            ?>
         </td>
     </tr>
 
